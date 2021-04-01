6a8bb287416815648ee64317348b883a2d7e8b88
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-04-01 22:15:20 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int del = number % 2;
+
+            if (del == 1)
+            {
+                Console.WriteLine("Число не четное " );
+            }
+                
+            else
+            {
+                Console.WriteLine("Число  четное " );
+            }
+
+        }
+    }
+}
